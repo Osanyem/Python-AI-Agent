01a9c2e842d46ba1c1e53c43725f8ab9662c13ab
Osanyem Osadebe <osanyemo@gmail.com>
2025-08-20 20:51:03 -0400

    Add --verbose flag and refactor

--- main.py
+++ main.py
@@ -4,23 +4,47 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
+    client = genai.Client(api_key=api_key)
+    is_verbose, user_args = get_user_input()
 
-try:
-    user_prompt = sys.argv[1]
+    if not user_args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(user_args)
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+
+    generate_content(client, messages, is_verbose)
+
+
+def get_user_input():
+    is_verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    return (is_verbose, args)
+
+
+def generate_content(client, messages, is_verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", contents=messages
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-except IndexError as e:
-    print(e)
-    sys.exit(1)
+    if is_verbose:
+        print(f"User prompt: {messages}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
+if __name__ == "__main__":
+    main()
