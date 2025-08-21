1544fba987dfea2a71262b373c6295f457b4b670
Osanyem Osadebe <osanyemo@gmail.com>
2025-08-20 20:12:14 -0400

    Add API key in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from python-ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
