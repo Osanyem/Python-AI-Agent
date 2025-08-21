e31fdfb7e344a431304ea9f98aa568e1b6cde845
Osanyem Osadebe <osanyemo@gmail.com>
2025-08-20 20:23:32 -0400

    Allow user to pass prompt in CLI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,11 +8,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=prompt
-)
-
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+try:
+    prompt = sys.argv[1]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=prompt
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+except IndexError as e:
+    print(e)
+    sys.exit(1)
